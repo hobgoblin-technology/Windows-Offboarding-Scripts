248bd8fba3832b9c6acdfd3b90659019f3272ea9
hobgoblin <95981171+hobgoblin-technology@users.noreply.github.com>
2026-05-22 09:18:46 -0700

    Update Remove-SharedMailboxes.ps1
    Added Distribution List cleanup

--- Remove-SharedMailboxes.ps1
+++ Remove-SharedMailboxes.ps1
@@ -12,7 +12,12 @@ param(
 Import-Module ExchangeOnlineManagement
 Connect-ExchangeOnline
 
+# Get all mailboxes
 $Mailboxes = Get-Mailbox -ResultSize Unlimited
+
+
+# Resolve the exact address (handles case‑insensitivity)
+$target = $User.Trim().ToLowerInvariant()
 
 
 # ==============================
@@ -27,8 +32,13 @@ Where-Object {
     $_.AccessRights -contains "FullAccess"
 } |
 ForEach-Object {
-    Write-Host "Removing FullAccess from $($_.Identity)" -ForegroundColor Yellow
-
+    if ($WhatIf){
+	Write-Host "[WHATIF] Would Remove FullAccess from $($_.Identity)" -ForegroundColor Yellow
+    }
+    else{
+   	Write-Host "Removing FullAccess from $($_.Identity)" -ForegroundColor Yellow
+    }
+    
     Remove-MailboxPermission `
         -Identity $_.Identity `
         -User $User `
@@ -47,8 +57,12 @@ Where-Object {
     $_.AccessRights -contains "SendAs"
 } |
 ForEach-Object {
-    Write-Host "Removing SendAs from $($_.Identity)" -ForegroundColor Yellow
-
+    if ($WhatIf){
+	Write-Host "[WHATIF] Would Remove SendAs from $($_.Identity)" -ForegroundColor Yellow
+    }
+    else{
+   	Write-Host "Removing SendAs from $($_.Identity)" -ForegroundColor Yellow
+    }
     Remove-RecipientPermission `
         -Identity $_.Identity `
         -Trustee $User `
@@ -67,14 +81,40 @@ Where-Object {
     $_.GrantSendOnBehalfTo -contains $User
 } |
 ForEach-Object {
-    Write-Host "Removing Send on Behalf from $($_.Identity)" -ForegroundColor Yellow
-
     if (-not $WhatIf) {
+	Write-Host "Removing Send on Behalf from $($_.Identity)" -ForegroundColor Yellow
         Set-Mailbox $_.Identity `
             -GrantSendOnBehalfTo @{Remove=$User}
     } else {
-        Write-Host "WHATIF: Would remove Send on Behalf from $($_.Identity)" -ForegroundColor DarkYellow
+        Write-Host "[WHATIF] Would remove Send on Behalf from $($_.Identity)" -ForegroundColor DarkYellow
     }
 }
 
+# ==============================
+# DISTRIBUTION LIST PERMISSIONS
+# ==============================
+ 
+Try {
+    #Get All Distribution Lists - Excluding Mail enabled security groups
+    $DistributionGroups = Get-Distributiongroup -resultsize unlimited |  Where {!$_.GroupType.contains("SecurityEnabled")}
+ 
+    #Loop through each Distribution Lists
+    ForEach ($Group in $DistributionGroups)
+    {
+        #Check if the Distribution List contains the particular user
+        If ((Get-DistributionGroupMember $Group.GUID -ResultSize Unlimited | Select -Expand PrimarySmtpAddress) -contains $User)
+        {
+            If (-not $WhatIf) {
+                Remove-DistributionGroupMember -Identity $Group.GUID -Member $User -Confirm:$false
+                Write-host "Removed user from group '$Group'" -f Green
+            }
+            Else{
+                Write-Host -f DarkYellow "[WHAT IF] Would remove '$User' from '$Group'"
+            }
+        }
+    }
+}
+Catch {
+    write-host -f Red "Error:" $_.Exception.Message
+}
 Write-Host "Permission cleanup complete." -ForegroundColor Green
